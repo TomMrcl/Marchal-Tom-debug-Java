def7889100bad3efb0eddaa75dc449e1c7fa885f
TomMrcl <tomarchal02@gmail.com>
2026-08-27 14:52:06 +0200

    Refactor analytics counter into separate methods

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,44 +1,49 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
 
-  private static int headacheCount = 0;
-  private static int rashCount = 0;
-  private static int pupilCount = 0;
+  private ISymptomReader reader;
+  private ISymptomWriter writer;
 
-  public static void main(String args[]) throws Exception {
-    // first get input
-    BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
-    String line = reader.readLine();
+  public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
+    this.reader = reader;
+    this.writer = writer;
+  }
 
-    int i = 0;
+  public List<String> getSymptoms() {
+    return reader.GetSymptoms();
+  }
 
-    while (line != null) {
-      i++;
-      System.out.println("symptom from file: " + line);
-      if (line.equals("headache")) {
-        headacheCount++;
-        System.out.println("number of headaches: " + headacheCount);
-      } else if (line.equals("rash")) {
-        rashCount++;
-        System.out.println("number of rashes: " + rashCount);
-      } else if (line.contains("pupils")) {
-        pupilCount++;
-        System.out.println("number of dilated pupils: " + pupilCount);
-      }
+  public Map<String, Integer> countSymptoms(List<String> symptoms) {
+    Map<String, Integer> symptomCounts = new HashMap<>();
 
-      line = reader.readLine(); // get another symptom
+    for (String symptom : symptoms) {
+      symptomCounts.put(symptom, symptomCounts.getOrDefault(symptom, 0) + 1);
     }
 
-    // next generate output
-    FileWriter writer = new FileWriter("result.out");
-    writer.write("headache: " + headacheCount + "\n");
-    writer.write("rash: " + rashCount + "\n");
-    writer.write("dialated pupils: " + pupilCount + "\n");
-    writer.close();
+    return symptomCounts;
+  }
+
+  public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
+    return new TreeMap<>(symptoms);
+  }
+
+  public void writeSymptoms(Map<String, Integer> symptoms) {
+    writer.writeSymptoms(symptoms);
+  }
+
+  public static void main(String[] args) {
+    AnalyticsCounter analyticsCounter = new AnalyticsCounter(
+        new ReadSymptomDataFromFile("symptoms.txt"),
+        new WriteSymptomDataToFile());
+    List<String> symptoms = analyticsCounter.getSymptoms();
+    Map<String, Integer> symptomCounts = analyticsCounter.countSymptoms(symptoms);
+    Map<String, Integer> sortedSymptoms = analyticsCounter.sortSymptoms(symptomCounts);
+    analyticsCounter.writeSymptoms(sortedSymptoms);
   }
 }
